a6fad6c4d4e82eaf232aebfa742b2ccd5d7aa6ba
unknown <brychan.thomas@btinternet.com>
2020-06-18 21:57:23 +0100

    Started creating Agent and GeneticAlgorithm classes

--- genetic algorithm/genetic-algorithm.js
+++ genetic algorithm/genetic-algorithm.js
@@ -25,3 +25,48 @@ class LogisticRegressionClassifier {
     return (output >= this.threshold ? true : false);
   }
 }
+
+class Agent {
+  constructor() {
+    this.player = new Player(game);
+    //2 parameters - whether there is grass directly ahead and whether
+    //there is a rock directly ahead
+    this.classifier = new LogisticRegressionClassifier(2);
+  }
+
+  decide() {
+    
+  }
+}
+
+class GeneticAlgorithm {
+  constructor(numOfAgents, mutationProb, fitnessFunction, game) {
+    this.game = game;
+    this.mutationProb = mutationProb;
+    this.fitnessFunction = fitnessFunction;
+    this.agents = [];
+    for (let i=0; i<numOfAgents; i++) {
+      this.agents.push(new Agent());
+    }
+  }
+
+  crossover(agent1, agent2) {
+    let crossoverIndex = Math.floor(Math.random() * agent1.classifier.weights.length);
+    let weights = agent1.classifier.weights.splice(0, crossoverIndex);
+    weights = weights.concat(agent2.classifier.weights.splice(crossoverIndex));
+    agent1.weights = weights;
+    return agent1;
+  }
+
+  mutate(agent) {
+    if (Math.random() < this.mutationProb) {
+      let replaceIndex = Math.floor(Math.random() * (agent1.classifier.weights.length+1));
+      agent.classifier.weights[replaceIndex] = (Math.random()*2) - 1;
+    }
+    return agent;
+  }
+
+  findFitnesses() {
+
+  }
+}
